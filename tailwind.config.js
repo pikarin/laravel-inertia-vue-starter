7126d34d48051ef964c783e47d54e5db56676a9e
Muhammad Aditia Firmansyah <aditia.pikarin@gmail.com>
2022-01-29 22:14:46 +0700

    update packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
     content: [
@@ -17,4 +17,4 @@ module.exports = {
     },
 
     plugins: [require('@tailwindcss/forms')],
-};
+}
